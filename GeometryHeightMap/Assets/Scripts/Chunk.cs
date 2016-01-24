ab17177d3844f419a10a0542340c583df2a72a06
Sean Coffey <sean@coffeyhouse.com>
2016-01-24 16:25:08 -0500

    In Progress

---
+++ GeometryHeightMap/Assets/Scripts/Chunk.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chunk {
+
+    public int size;
+    public float heightScale;
+    public GameObject prefab;
+
+    public int chunkX;
+    public int chunkY;
+
+    public float[,] values;
+    public SmartTile[,] tiles;
+
+	public Chunk(int x, int y, int iSize, int iHeightsScale, GameObject iPrefab)
+    {
+        chunkX = x;
+        chunkY = y;
+        size = iSize;
+        values = new float[size, size];
+        tiles = new SmartTile[size, size];
+    }
+
+    public void AddValue(int x, int y, float value)
+    {
+        values[chunkX - x, chunkY - y] = value;
+    }
+
+    public void AddTile(int x, int y, SmartTile newTile)
+    {
+        tiles[x, y] = newTile;
+    }
+
+    public void LoadTiles()
+    {
+        GameObject temp = (GameObject)GameObject.Instantiate(prefab, new Vector3(), Quaternion.identity);
+    }
+
+    public void UpdateVisuals()
+    {
+        foreach (SmartTile tile in tiles)
+        {
+            tile.updateVisuals();
+        }
+    }
+}
--- GeometryHeightMap/Assets/Scripts/SmartGeometryMap.cs
+++ GeometryHeightMap/Assets/Scripts/SmartGeometryMap.cs
@@ -9,12 +9,15 @@ public class SmartGeometryMap : MonoBehaviour {
     public float heightScale;
     public GameObject tile;
 
+    public int chunkSize;
+
     public float maxDistance;
     public float minDistance;
 
     public GameObject focus;
 
-    public SmartTile[,] tiles;
+    public float[,] values;
+    public List<Chunk> chunks;
 
     void Start()
     {
@@ -24,16 +27,14 @@ public class SmartGeometryMap : MonoBehaviour {
         int width = (int)(heightmap.width * scale);
         int height = (int)(heightmap.height * scale);
 
-        tiles = new SmartTile[width, height];
+        values = new float[width, height];
+        chunks = new List<Chunk>();
 
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                GameObject temp = (GameObject)Instantiate(tile, new Vector3(x, (heightmap.GetPixel((int)(x / scale), (int)(y / scale)).r * heightScale) / 2, y), Quaternion.identity);
-                temp.GetComponent<SmartTile>().height = heightScale;
-                temp.GetComponent<SmartTile>().focus = focus;
-                tiles[x,y] = temp.GetComponent<SmartTile>();
+                values[x, y] = (heightmap.GetPixel((int)(x / scale), (int)(y / scale)).r * heightScale);
             }
         }
     }
@@ -43,29 +44,27 @@ public class SmartGeometryMap : MonoBehaviour {
         SmartTile.maxDistance = this.maxDistance;
         SmartTile.minDistance = this.minDistance;
 
-        int range = (int)(maxDistance * 1.5f);
+        float range = maxDistance * 1.5f;
 
-        int maxX = (int)focus.transform.position.x + range;
-        int minX = (int)focus.transform.position.x - range;
-        int maxY = (int)focus.transform.position.z + range;
-        int minY = (int)focus.transform.position.z - range;
+        float maxX = focus.transform.position.x + range;
+        float minX = focus.transform.position.x - range;
+        float maxY = focus.transform.position.z + range;
+        float minY = focus.transform.position.z - range;
 
-        if (maxX > tiles.GetLength(0)) maxX = tiles.GetLength(0);
+        if (maxX > values.GetLength(0)) maxX = values.GetLength(0);
         if (minX < 0) minX = 0;
-        if (maxY > tiles.GetLength(1)) maxY = tiles.GetLength(1);
+        if (maxY > values.GetLength(1)) maxY = values.GetLength(1);
         if (minY < 0) minY = 0;
 
-        //minX = 0;
-        //maxX = tiles.GetLength(0);
-        //minY = 0;
-        //maxY = tiles.GetLength(0);
-
-        for (int x = minX; x < maxX; x++)
-        {
-            for (int y = minY; y < maxY; y++)
-            {
-                tiles[x,y].updateVisuals();
-            }
-        }
+        //for (int x = (int)minX; x < maxX; x++)
+        //{
+        //    for (int y = (int)minY; y < maxY; y++)
+        //    {
+        //        if (chunks.con != null)
+        //        {
+        //
+        //        }
+        //    }
+        //}
     }
 }
